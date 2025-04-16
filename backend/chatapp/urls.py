0e7300d10889ca862839046fbae8b9683145ac21
ebenissan <93489382+ebenissan@users.noreply.github.com>
2025-04-16 17:54:29 -0500

    title updating but second empty chat being created

--- backend/chatapp/urls.py
+++ backend/chatapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ChatSessionViewSet, chatbot_api_view, store_message, chatbot_stream_view, get_messages_by_session
+from .views import ChatSessionViewSet, chatbot_api_view, store_message, chatbot_stream_view, get_messages_by_session, update_chat_title
 
 router = DefaultRouter()
 router.register(r'', ChatSessionViewSet, basename='chatapp')
@@ -9,7 +9,7 @@ urlpatterns = [
     path('chatbot/', chatbot_api_view, name='chatbot'),
     path('store_message/', store_message, name='store_message'),
     path("<uuid:session_id>/messages/", get_messages_by_session, name="get_messages_by_session"),
-
+    path("<uuid:session_id>/", update_chat_title),
     # path("chatbot/stream/", chatbot_stream_view, name="chatbot-stream"),
     path('', include(router.urls)),  # adds /api/chatapp/ endpoints
 ]
